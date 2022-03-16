51ee21a0038fdbf4b9eae610b535e46c953acde1
tariktesfa <tariktesfa9090@gmail.com>
2022-03-16 11:52:06 +0300

    Update config file

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import itemsReducer from './Data/CovidData';
+import DataReducer from './Data/CovidData';
 
 const reducer = combineReducers({
-  items: itemsReducer,
+  items: DataReducer,
 });
 const store = createStore(reducer, applyMiddleware(logger, thunk));
 export default store;
