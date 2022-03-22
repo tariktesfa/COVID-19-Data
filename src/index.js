f0c5826e8bb42ab005f4c1f24460270f0981f32e
tariktesfa <tariktesfa9090@gmail.com>
2022-03-22 16:25:09 +0300

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import App from './App';
-import store from './redux/configureStore';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-      ,
-    </BrowserRouter>
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root'),
 );
