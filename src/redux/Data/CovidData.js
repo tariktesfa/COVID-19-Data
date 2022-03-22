b245a25edd289d2240698a2195478ff7473c7586
tariktesfa <tariktesfa9090@gmail.com>
2022-03-22 10:48:17 +0300

    Fetch covid data

--- src/redux/Data/CovidData.js
+++ src/redux/Data/CovidData.js
@@ -1,22 +1,15 @@
-const FETCHDATA = 'COVID-19-DATA/DATA/FETCH';
-export const date = new Date().toISOString().split('T')[0];
-export const newDate = date.toString();
-const url = `https://api.covid19tracking.narrativa.com/api/${date}`;
+const FETCHDATA = 'FETCHDATA';
 
-const fetchData = (payload) => ({
+const initialState = [];
+
+const dataURL = 'https://api.covid19api.com/summary';
+
+export const getData = (payload) => ({
   type: FETCHDATA,
   payload,
 });
 
-export const fetchItemData = () => async (dispatch) => {
-  const response = await fetch(url);
-  const data = await response.json();
-  const dateItems = data.dates[newDate].countries;
-  const obj = Object.values(dateItems);
-  dispatch(fetchData(obj));
-};
-
-const DataReducer = (state = [], action) => {
+const CovidData = (state = initialState, action) => {
   switch (action.type) {
     case FETCHDATA:
       return action.payload;
@@ -24,4 +17,25 @@ const DataReducer = (state = [], action) => {
       return state;
   }
 };
-export default DataReducer;
+
+export const loadDataCountries = () => async (dispatch) => {
+  const response = await fetch(dataURL);
+  const data = await response.json();
+  const allData = data.Countries.map((e) => {
+    const currentData = {
+      id: e.ID,
+      country: e.Country,
+      NewConfirmed: e.NewConfirmed,
+      TotalConfirmed: e.TotalConfirmed,
+      NewDeaths: e.NewDeaths,
+      TotalDeaths: e.TotalDeaths,
+      NewRecovered: e.NewRecovered,
+      TotalRecovered: e.TotalRecovered,
+      Date: e.Date,
+    };
+    return currentData;
+  });
+  dispatch(getData(allData));
+};
+
+export default CovidData;
